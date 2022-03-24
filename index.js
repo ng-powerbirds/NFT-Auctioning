8d32135d4307b7e9d67e061f4539e1237cfd26ee
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-24 13:10:58 +0300

    code cleanup

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ import {loadStdlib} from '@reach-sh/stdlib';//load stlib as reach
 
 const reach = loadStdlib(process.env);
 
-const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};//On these lines we define a few helpful constants and defaults for later, some corresponding to the enumerations we defined in Reach.
-const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
+// const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};//On these lines we define a few helpful constants and defaults for later, some corresponding to the enumerations we defined in Reach.
+// const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
 const {standardUnit} = reach;
 const defaults = {defaultFundAmt: '10', defaultWager: '3', standardUnit};
 
@@ -39,58 +39,58 @@ class App extends React.Component {
   render() { return renderView(this, AppViews); }//we render the appropriate view from rps-9-web/views/AppViews.js.
 }
 
-class Player extends React.Component {
-  random() { return reach.hasRandom.random(); }
-  async getHand() { // Fun([], UInt)
-    const hand = await new Promise(resolveHandP => {
-      this.setState({view: 'GetHand', playable: true, resolveHandP});
-    });
-    this.setState({view: 'WaitingForResults', hand});
-    return handToInt[hand];
-  }
-  seeOutcome(i) { this.setState({view: 'Done', outcome: intToOutcome[i]}); }
-  informTimeout() { this.setState({view: 'Timeout'}); }
-  playHand(hand) { this.state.resolveHandP(hand); }
-}
+// class Player extends React.Component {
+//   random() { return reach.hasRandom.random(); }
+//   async getHand() { // Fun([], UInt)
+//     const hand = await new Promise(resolveHandP => {
+//       this.setState({view: 'GetHand', playable: true, resolveHandP});
+//     });
+//     this.setState({view: 'WaitingForResults', hand});
+//     return handToInt[hand];
+//   }
+//   seeOutcome(i) { this.setState({view: 'Done', outcome: intToOutcome[i]}); }
+//   informTimeout() { this.setState({view: 'Timeout'}); }
+//   playHand(hand) { this.state.resolveHandP(hand); }
+// }
 
-class Deployer extends Player {
-  constructor(props) {
-    super(props);
-    this.state = {view: 'SetWager'};
-  }
-  setWager(wager) { this.setState({view: 'Deploy', wager}); }
-  async deploy() {
-    const ctc = this.props.acc.contract(backend);
-    this.setState({view: 'Deploying', ctc});
-    this.wager = reach.parseCurrency(this.state.wager); // UInt
-    this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt
-    backend.Alice(ctc, this);
-    const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
-    this.setState({view: 'WaitingForAttacher', ctcInfoStr});
-  }
-  render() { return renderView(this, DeployerViews); }
-}
-class Attacher extends Player {
-  constructor(props) {
-    super(props);
-    this.state = {view: 'Attach'};
-  }
-  attach(ctcInfoStr) {
-    const ctc = this.props.acc.contract(backend, JSON.parse(ctcInfoStr));
-    this.setState({view: 'Attaching'});
-    backend.Bob(ctc, this);
-  }
-  async acceptWager(wagerAtomic) { // Fun([UInt], Null)
-    const wager = reach.formatCurrency(wagerAtomic, 4);
-    return await new Promise(resolveAcceptedP => {
-      this.setState({view: 'AcceptTerms', wager, resolveAcceptedP});
-    });
-  }
-  termsAccepted() {
-    this.state.resolveAcceptedP();
-    this.setState({view: 'WaitingForTurn'});
-  }
-  render() { return renderView(this, AttacherViews); }
-}
+// class Deployer extends Player {
+//   constructor(props) {
+//     super(props);
+//     this.state = {view: 'SetWager'};
+//   }
+//   setWager(wager) { this.setState({view: 'Deploy', wager}); }
+//   async deploy() {
+//     const ctc = this.props.acc.contract(backend);
+//     this.setState({view: 'Deploying', ctc});
+//     this.wager = reach.parseCurrency(this.state.wager); // UInt
+//     this.deadline = {ETH: 10, ALGO: 100, CFX: 1000}[reach.connector]; // UInt
+//     backend.Alice(ctc, this);
+//     const ctcInfoStr = JSON.stringify(await ctc.getInfo(), null, 2);
+//     this.setState({view: 'WaitingForAttacher', ctcInfoStr});
+//   }
+//   render() { return renderView(this, DeployerViews); }
+// }
+// class Attacher extends Player {
+//   constructor(props) {
+//     super(props);
+//     this.state = {view: 'Attach'};
+//   }
+//   attach(ctcInfoStr) {
+//     const ctc = this.props.acc.contract(backend, JSON.parse(ctcInfoStr));
+//     this.setState({view: 'Attaching'});
+//     backend.Bob(ctc, this);
+//   }
+//   async acceptWager(wagerAtomic) { // Fun([UInt], Null)
+//     const wager = reach.formatCurrency(wagerAtomic, 4);
+//     return await new Promise(resolveAcceptedP => {
+//       this.setState({view: 'AcceptTerms', wager, resolveAcceptedP});
+//     });
+//   }
+//   termsAccepted() {
+//     this.state.resolveAcceptedP();
+//     this.setState({view: 'WaitingForTurn'});
+//   }
+//   render() { return renderView(this, AttacherViews); }
+// }
 
 renderDOM(<App />);
