47da36e0c9dfb645abc572b1b001abd7ed7106ae
timothyAgevi <timothyagevi@gmail.com>
2022-03-26 16:25:55 +0300

    required cli output

---
+++ index.mjs
@@ -0,0 +1,34 @@
+//desired output f
+
+/*Creator has 99.999 ALGO and 1 of the NFT
+Creator sets parameters of sale
+Bob has 100 ALGO and 0 of the NFT
+Bob decides to bid 5.879184
+Carla has 100 ALGO and 0 of the NFT
+Carla decides to bid 0.680133
+Alice has 100 ALGO and 0 of the NFT
+Alice decides to bid 9.918794
+Bob sees that the NFT is 6, the reserve price is 2, and that they have until 38 to bid
+Carla sees that the NFT is 6, the reserve price is 2, and that they have until 38 to bid
+Alice sees that the NFT is 6, the reserve price is 2, and that they have until 38 to bid
+Carla does not bid because 2 is too high
+Bob bids 5.879184 against 2
+Alice bids 9.918794 against 2
+Carla does not bid because 5.879184 is too high
+Creator saw that 2F4HFE5QJXN6D4ZC7OZ332JDTQHHERCVN44Y3GBSJMSX64MNHN3AYXJASA bid 5.879184
+Alice bids 9.918794 against 5.879184
+Bob does not bid because 9.918794 is too high
+Creator saw that Y43GY3ESHUL3CTL7G3PUUO3Z5F7TUEJU65P5YLJURP2SCXFU2OBQLXN42M bid 9.918794
+Carla does not bid because 9.918794 is too high
+Creator observes the auction has hit the timeout
+Creator observes the auction has hit the timeout
+Creator saw that Y43GY3ESHUL3CTL7G3PUUO3Z5F7TUEJU65P5YLJURP2SCXFU2OBQLXN42M won
+Creator has 109.904794 ALGO and 0 of the NFT
+Bob does not bid because 9.918794 is too high
+Bob saw that Y43GY3ESHUL3CTL7G3PUUO3Z5F7TUEJU65P5YLJURP2SCXFU2OBQLXN42M won
+Alice saw that Y43GY3ESHUL3CTL7G3PUUO3Z5F7TUEJU65P5YLJURP2SCXFU2OBQLXN42M won
+Bob has 99.996 ALGO and 0 of the NFT
+Alice has 90.077206 ALGO and 1 of the NFT
+Carla does not bid because 9.918794 is too high
+Carla saw that Y43GY3ESHUL3CTL7G3PUUO3Z5F7TUEJU65P5YLJURP2SCXFU2OBQLXN42M won
+Carla has 99.999099 ALGO and 0 of the NFT*/
