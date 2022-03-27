591b7fe3972639f1ccfc7c94ae5a713c26c68d7b
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-03-27 14:21:44 +0300

    rm non-existent imports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import AppViews from './views/AppViews';
-import DeployerViews from './views/DeployerViews';
-import AttacherViews from './views/AttacherViews';
+// import DeployerViews from './views/DeployerViews';
+// import AttacherViews from './views/AttacherViews';
 import {renderDOM, renderView} from './views/render';
 import './index.css';
 import * as backend from './build/index.main.mjs';
